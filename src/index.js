5cd4e7b78c1ac0e1852c403c9dd11039aa4d281c
poetri598 <poetri598@example.com>
2025-07-25 22:23:06 +0700

    25/07/2025:10:22:PM

--- src/index.js
+++ src/index.js
@@ -35,7 +35,7 @@ app.use(
 );
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "pusatkarirfticlient-production.up.railway.app",
     credentials: true,
   })
 );
